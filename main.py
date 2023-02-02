3aa1ef43012bbcfc72a093eb68b036cfcedd9625
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-02 14:07:02 +0200

    selenium imports setup

---
+++ main.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
