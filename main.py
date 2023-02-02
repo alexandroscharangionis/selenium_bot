3df205d68d68e44640fb88287cc77a22e20926c7
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-02-02 14:16:03 +0200

    form autocomplete logic

--- main.py
+++ main.py
@@ -13,3 +13,22 @@ options.add_experimental_option("detach", True)
 
 # Webdriver setup:
 driver = webdriver.Chrome(service=service, options=options)
+
+
+# -------------------------- MOCK-UP FORM SUBMISSION --------------------------
+driver.get(
+    "https://web.archive.org/web/20220120120408/https://secure-retreat-92358.herokuapp.com/")
+
+# Get element tags by tag names/ CSS selector:
+first_name = driver.find_element(By.NAME, "fName")
+last_name = driver.find_element(By.NAME, "lName")
+email = driver.find_element(By.NAME, "email")
+button = driver.find_element(By.CSS_SELECTOR, "form button")
+
+# Type strings into input:
+first_name.send_keys("Alexandros")
+last_name.send_keys("Charangionis")
+email.send_keys("alexandroscharangionis@gmail.com")
+
+# Submit form
+button.click()
